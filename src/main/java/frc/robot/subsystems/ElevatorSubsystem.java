d9496ee06f673be44c12ad8a4e86c4f28b5085c9
Nanson Chen <buyingsaucer2@gmail.com>
2025-01-30 17:31:24 -0500

    Relocated some responsibilities to the elevator command and stores related constants locally for easy access.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,23 +1,29 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
+    public static final Constants.Levels[] levels = Constants.Levels.values();
+    private static Constants.Levels currentLevel = Constants.Levels.HOME;
 
     public ElevatorSubsystem()
     {
         //Initializing electronics
     }
-
+    
     /**
-     * Attempts to change the level of the elevator.
-     * @param increaseElevator is true if the elevator should increase in level, false if the elevator should decrease in level.
-     * @return true if the elevator can change its level, false otherwise.
+     * Sets the elevator at the set level.
+     * @param level is a level the elevator should be set at.
      */
-    public boolean changeLevel(boolean increaseElevator)
-    {
-        //TODO unimplemented method
-        throw new UnsupportedOperationException("Unimplemented method 'changeLevel'");
+    public void setElevatorLevel(Constants.Levels level) {
+        //Figure out if the elevator should go up or down.
+        //Move elevator based upon previous condition until the corresponding limit switch is triggered.
+        //Update the current level.
     }
 
+    public Constants.Levels getCurrentLevel()
+    {
+        return currentLevel;
+    }
 }
