134febb4d5a497319a6fa00e0548053bc0a38be0
Nanson Chen <buyingsaucer2@gmail.com>
2025-02-06 19:25:14 -0500

    Added comments

--- src/main/java/frc/robot/commands/ElevatorAutoHomeCommand.java
+++ src/main/java/frc/robot/commands/ElevatorAutoHomeCommand.java
@@ -10,22 +10,35 @@ public class ElevatorAutoHomeCommand extends Command{
 
     private ElevatorSubsystem _ElevatorSubsystem;
     
+    /**
+     * Creates a new ElevatorAutoHomeCommand.
+     * @param ElevatorSubsystem is the elevator subsystem used by this command.
+     */
     public ElevatorAutoHomeCommand(ElevatorSubsystem ElevatorSubsystem){
         _ElevatorSubsystem = ElevatorSubsystem;
         addRequirements(_ElevatorSubsystem);
     }
 
+    /**
+     * Starts by moving the elevator down.
+     */
     @Override
     public void initialize() {
         _ElevatorSubsystem.elevatorLeftMotor.set(-0.1);
         _ElevatorSubsystem.elevatorRightMotor.set(-0.1);
     }
 
+    /**
+     * Command is finished and its end method is run when the bottom limit switch is pressed.
+     */
     @Override
     public boolean isFinished() {
         return !_ElevatorSubsystem.bottomlimitSwitch.get();
     }
 
+    /**
+     * Stops the elevator motors and stores the encoders and home positions of the motors.
+     */
     @Override
     public void end(boolean interrupted) {
         _ElevatorSubsystem.elevatorLeftMotor.set(0.0);
