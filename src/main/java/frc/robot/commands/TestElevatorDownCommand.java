bcdb362916b7f84cc22ed7247adcb4b9d7ff8211
Nanson Chen <buyingsaucer2@gmail.com>
2025-02-06 19:18:50 -0500

    Created a new down command

---
+++ src/main/java/frc/robot/commands/TestElevatorDownCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import frc.robot.Constants.ElevatorLevels;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class TestElevatorDownCommand extends ElevatorGoToLevelCommand
+{
+    /**
+     * Creates a new TestElevatorDownCommand.
+     * Attempts to move the elevator down one level if possible.
+     * @param elevatorSubsystem The elevator subsystem used by this command.
+     */
+    public TestElevatorDownCommand(ElevatorSubsystem elevatorSubsystem) {
+        super(elevatorSubsystem, getLevel(elevatorSubsystem.getLevel()));
+    }
+
+    /**
+     * Gets the previous level of the elevator.
+     * @param currentLevel is the current level of the elevator.
+     * @return the previous level of the elevator, returns NONE if the level of the elevator cannot be decreased.
+     */
+    private static ElevatorLevels getLevel(ElevatorLevels currentLevel) {
+        if(validToMoveDown(currentLevel))
+            return ElevatorLevels.values()[currentLevel.ordinal() - 1];
+        return ElevatorLevels.NONE;
+    }
+
+    /**
+     * Checks if the elevator is not at the bottom level.
+     * @param currentLevel The current level of the elevator.
+     * @return true if the elevator is not at the bottom level, false otherwise.
+     */
+    private static boolean validToMoveDown(ElevatorLevels currentLevel) {
+        return currentLevel != ElevatorLevels.HOME;
+    }
+}
