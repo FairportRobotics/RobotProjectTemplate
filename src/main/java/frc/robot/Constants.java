b69b8f7e8a6646de2ef6f3ee7e46554d4bacd617
Grey <Gjdulisse@gmail.com>
2025-02-11 07:35:30 -0500

    Formated Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,63 +5,74 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-
 
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
 
-  
-  public static class ArmConstants{
+  public static class ArmConstants {
 
     public static final int MotorYID = 0;
     public static final int LimitID = 0;
+
     /**
-   * The ArmPositions Enum is used to store positons for the arm. There are 4 values.
-   * <ul>
-   * <li> UP </l1>
-   * <li> MIDDLE </l1>
-   * <li> DOWN </l1>
-   * <li> NONE </l1>
-   * </ul>
-   * Each has a double that can be accesed with ArmPositions.getValue. Each value only has one double. No more, no less. If thou shall try to get two, thou shall only get one, and one shall be the number given. Trying to get three is outright. 
-   * 
-   */
-    public enum ArmPositions{
+     * The ArmPositions Enum is used to store positons for the arm. There are 4
+     * values.
+     * <ul>
+     * <li>UP </l1>
+     * <li>MIDDLE </l1>
+     * <li>DOWN </l1>
+     * <li>NONE </l1>
+     * </ul>
+     * Each has a double that can be accesed with ArmPositions.getValue. Each value
+     * only has one double. No more, no less. If thou shall try to get two, thou
+     * shall only get one, and one shall be the number given. Trying to get three is
+     * outright.
+     * 
+     */
+    public enum ArmPositions {
       UP(1),
       MIDDLE(2),
       DOWN(3),
       NONE(0);
+
       double wa;
-      private ArmPositions(double value){
+
+      private ArmPositions(double value) {
         this.wa = value;
-        }
+      }
+
       /**
        * Get the value of the ArmPositions Object
+       * 
        * @return A double that is to be used for seting the position of the arm.
        */
-      public double getValue(){
+      public double getValue() {
         return wa;
       }
+    }
+
+    public static class AdvantageKitConstants {
+      public enum RobotType {
+        SIM,
+        REAL,
+        REPLAY
       }
 
-  public static class AdvantageKitConstants{
-    public enum RobotType{
-      SIM,
-      REAL,
-      REPLAY
+      public static final RobotType CURRENT_MODE = RobotType.REAL;
     }
-    public static final RobotType CURRENT_MODE = RobotType.REAL;
-  }
 
-  
   }
 }
