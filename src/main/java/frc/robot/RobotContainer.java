2827a6ee1def34b6ea656556947fcc8ae3f4aaeb
BrickLayer22 <mason.kemp.silva@icloud.com>
2025-02-11 19:51:50 -0500

    Code should work inorder to make the hand work :)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.ElevatorLevelCommand;
@@ -77,7 +78,7 @@ public class RobotContainer {
 
         // Controlls if the lego hand intakes or outakes
         operator.x().onTrue(new HandCommand(handSubsystem, .5));
-        operator.y().onTrue(new Wait(handSubsystem));
+        operator.y().onTrue(Commands.deadline(new WaitCommand(5.0), new HandCommand(handSubsystem, -.5)));
 
         // Sets the pov up and down of operator's controller to increase or decrease the level of the arm elevator.
         operator.povUp().onTrue(new ElevatorLevelCommand(elevatorSubsystem, true));
