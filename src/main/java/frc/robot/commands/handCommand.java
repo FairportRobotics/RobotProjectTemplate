0fce8cf39b8e68dea9a0b4b5aea7bb12cce6fbf5
Nanson Chen <buyingsaucer2@gmail.com>
2025-02-15 15:41:57 -0500

    handCommand is now HandCommand.

--- src/main/java/frc/robot/commands/handCommand.java
+++ src/main/java/frc/robot/commands/handCommand.java
@@ -4,52 +4,47 @@
 
 package frc.robot.commands;
 
-
 import frc.robot.subsystems.HandSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
-
 import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.revrobotics.spark.SparkBase.ControlType;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
- 
 
-public class handCommand extends Command{
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+public class HandCommand extends Command {
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
-    HandSubsystem _HandSubsystem;
-    double speed;
-    VelocityVoltage intakeRequest;
-    public DigitalInput handLimitSwitch = new DigitalInput(1);
-   /**
+  HandSubsystem _HandSubsystem;
+  double speed;
+  VelocityVoltage intakeRequest;
+  public DigitalInput handLimitSwitch = new DigitalInput(1);
+
+  /**
    * Creates a new HandCommand.
-   *  HandCommand causees the neo in the hand to spin off the given value either intakeing or outakeing
+   * HandCommand causees the neo in the hand to spin off the given value either
+   * intakeing or outakeing
+   * 
    * @param subsystem The HandSubsystem.
    */
-    public handCommand(HandSubsystem handSubsystem, double intakeSpeed){
-        _HandSubsystem = handSubsystem;
-        speed = intakeSpeed;
-        addRequirements(_HandSubsystem);
-    }
+  public HandCommand(HandSubsystem handSubsystem, double intakeSpeed) {
+    _HandSubsystem = handSubsystem;
+    speed = intakeSpeed;
+    addRequirements(_HandSubsystem);
+  }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-      _HandSubsystem.setSpeed(speed);
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    _HandSubsystem.setSpeed(speed);
+  }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
     return handLimitSwitch.get();
-    }
+  }
 
-    //Ends the movement of the motor
-    @Override
-    public void end(boolean interrupted) {
-        _HandSubsystem.setSpeed(0.0);
-    }
+  // Ends the movement of the motor
+  @Override
+  public void end(boolean interrupted) {
+    _HandSubsystem.setSpeed(0.0);
+  }
 }
