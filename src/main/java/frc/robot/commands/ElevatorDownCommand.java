8c5a304c9a20188984fe63adb94abef133add9c3
Nanson Chen <buyingsaucer2@gmail.com>
2025-02-15 15:39:53 -0500

    Deleted the redundant commands and replaced them with the test commands.

--- src/main/java/frc/robot/commands/ElevatorDownCommand.java
+++ src/main/java/frc/robot/commands/ElevatorDownCommand.java
@@ -1,106 +1,45 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ElevatorLevels;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-public class ElevatorDownCommand extends Command{
-
-    private ElevatorSubsystem _ElevatorSubsystem;
-    private double requestPosRots = Double.MAX_VALUE;
-
-    private StatusSignal<Angle> leftPosition;
-    private StatusSignal<Angle> rightPosition;
-
-    private StatusSignal<Double> leftPosError;
-    private StatusSignal<Double> rightPosError;
-
-    final PositionVoltage rightPositionRequest;
-    final PositionVoltage leftPositionRequest;
-
-    public ElevatorLevels pos;
-
-    public ElevatorDownCommand(ElevatorSubsystem ElevatorSubsystem){
-        pos = ElevatorSubsystem.getGoToLevel();
-        if (pos != ElevatorLevels.HOME) {
-            pos = ElevatorLevels.values()[pos.ordinal() - 1];
-        }
-        _ElevatorSubsystem = ElevatorSubsystem;
-        addRequirements(_ElevatorSubsystem);
-
-        requestPosRots = getEncoderValueForLevel(pos);
-
-        rightPosition = _ElevatorSubsystem.getRightMotor().getPosition();
-        leftPosition = _ElevatorSubsystem.getLeftMotor().getPosition();
-
-        leftPosError = _ElevatorSubsystem.getLeftMotor().getClosedLoopError();
-        rightPosError = _ElevatorSubsystem.getRightMotor().getClosedLoopError();
-
-        rightPositionRequest = new PositionVoltage(0).withSlot(0);
-        leftPositionRequest = new PositionVoltage(0).withSlot(0);
+public class ElevatorDownCommand extends ElevatorGoToLevelCommand {
+    /**
+     * Creates a new TestElevatorDownCommand.
+     * Attempts to move the elevator down one level if possible.
+     * 
+     * @param elevatorSubsystem The elevator subsystem used by this command.
+     */
+    public ElevatorDownCommand(ElevatorSubsystem elevatorSubsystem) {
+        super(elevatorSubsystem);
     }
 
     @Override
-    public void initialize() {
-        _ElevatorSubsystem.getLeftMotor().setNeutralMode(NeutralModeValue.Coast);
-        _ElevatorSubsystem.getRightMotor().setNeutralMode(NeutralModeValue.Coast);
-
-        _ElevatorSubsystem.getLeftMotor()
-                .setControl(leftPositionRequest.withPosition(_ElevatorSubsystem.getLeftHomePos() + requestPosRots));
-        _ElevatorSubsystem.getRightMotor()
-                .setControl(rightPositionRequest.withPosition(_ElevatorSubsystem.getRightHomePos() + requestPosRots));
+    public void execute() {
+        goToLevel = getLevel(elevatorSubsystem.getGoToLevel());
+        super.execute();
     }
 
-    @Override
-    public boolean isFinished() {
-        leftPosError.refresh();
-        rightPosError.refresh();
-
-        if (requestPosRots <= 0) {
-            return !_ElevatorSubsystem.getBottomLimitSwitchAsBoolean();
-        } else if (leftPosition.hasUpdated() && rightPosition.hasUpdated()) {
-
-            SmartDashboard.putNumber("Ele Left Pos", leftPosition.getValueAsDouble());
-            SmartDashboard.putNumber("Ele Right", rightPosition.getValueAsDouble());
-
-            return (Math.abs(leftPosition.getValueAsDouble() - (requestPosRots + _ElevatorSubsystem.getLeftHomePos())) <= 0.1
-                    ||
-                    Math.abs(rightPosition.getValueAsDouble()
-                            - (requestPosRots + _ElevatorSubsystem.getRightHomePos())) <= 0.1);
-        }
-
-        return false;
+    /**
+     * Gets the previous level of the elevator.
+     * 
+     * @param currentLevel is the current level of the elevator.
+     * @return the previous level of the elevator, returns NONE if the level of the
+     *         elevator cannot be decreased.
+     */
+    private static ElevatorLevels getLevel(ElevatorLevels currentLevel) {
+        if (validToMoveDown(currentLevel))
+            return ElevatorLevels.values()[currentLevel.ordinal() - 1];
+        return currentLevel;
     }
 
-    @Override
-    public void end(boolean interrupted) {
-        // RobotContainer.noteAquired = false;
-        _ElevatorSubsystem.getLeftMotor().stopMotor();
-        _ElevatorSubsystem.getRightMotor().stopMotor();
-        _ElevatorSubsystem.getLeftMotor().setNeutralMode(NeutralModeValue.Brake);
-        _ElevatorSubsystem.getRightMotor().setNeutralMode(NeutralModeValue.Brake);
-    }
-
-    public static double getEncoderValueForLevel(ElevatorLevels level) {
-        switch (level) {
-            case HOME:
-                return 0.0;
-            case ONE:
-                return 0.0;
-            case TWO:
-                return 0.0;
-            case THREE:
-                return 0.0;
-            case FOUR:
-                return 0.0;
-            default:
-                throw new IllegalArgumentException("Unknown level: " + level);
-        }
+    /**
+     * Checks if the elevator is not at the bottom level.
+     * 
+     * @param currentLevel The current level of the elevator.
+     * @return true if the elevator is not at the bottom level, false otherwise.
+     */
+    private static boolean validToMoveDown(ElevatorLevels currentLevel) {
+        return !ElevatorLevels.values()[0].equals(currentLevel);
     }
 }
--- src/main/java/frc/robot/commands/ElevatorUpCommand.java
+++ src/main/java/frc/robot/commands/ElevatorUpCommand.java
@@ -1,106 +1,45 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ElevatorLevels;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-public class ElevatorUpCommand extends Command{
-
-    private ElevatorSubsystem _ElevatorSubsystem;
-    private double requestPosRots = Double.MAX_VALUE;
-
-    private StatusSignal<Angle> leftPosition;
-    private StatusSignal<Angle> rightPosition;
-
-    private StatusSignal<Double> leftPosError;
-    private StatusSignal<Double> rightPosError;
-
-    final PositionVoltage rightPositionRequest;
-    final PositionVoltage leftPositionRequest;
-
-    public ElevatorLevels pos;
-
-    public ElevatorUpCommand(ElevatorSubsystem ElevatorSubsystem){
-        pos = ElevatorSubsystem.getGoToLevel();
-        if (pos != ElevatorLevels.FOUR) {
-            pos = ElevatorLevels.values()[pos.ordinal() + 1];
-        }
-        _ElevatorSubsystem = ElevatorSubsystem;
-        addRequirements(_ElevatorSubsystem);
-
-        requestPosRots = getEncoderValueForLevel(pos);
-
-        rightPosition = _ElevatorSubsystem.getRightMotor().getPosition();
-        leftPosition = _ElevatorSubsystem.getLeftMotor().getPosition();
-
-        leftPosError = _ElevatorSubsystem.getLeftMotor().getClosedLoopError();
-        rightPosError = _ElevatorSubsystem.getRightMotor().getClosedLoopError();
-
-        rightPositionRequest = new PositionVoltage(0).withSlot(0);
-        leftPositionRequest = new PositionVoltage(0).withSlot(0);
+public class ElevatorUpCommand extends ElevatorGoToLevelCommand {
+    /**
+     * Creates a new TestElevatorUpCommand.
+     * Attempts to move the elevator up one level if possible.
+     * 
+     * @param elevatorSubsystem The elevator subsystem used by this command.
+     */
+    public ElevatorUpCommand(ElevatorSubsystem elevatorSubsystem) {
+        super(elevatorSubsystem);
     }
 
     @Override
-    public void initialize() {
-        _ElevatorSubsystem.getLeftMotor().setNeutralMode(NeutralModeValue.Coast);
-        _ElevatorSubsystem.getRightMotor().setNeutralMode(NeutralModeValue.Coast);
-
-        _ElevatorSubsystem.getLeftMotor()
-                .setControl(leftPositionRequest.withPosition(_ElevatorSubsystem.getLeftHomePos() + requestPosRots));
-        _ElevatorSubsystem.getRightMotor()
-                .setControl(rightPositionRequest.withPosition(_ElevatorSubsystem.getRightHomePos() + requestPosRots));
+    public void execute() {
+        goToLevel = getLevel(elevatorSubsystem.getGoToLevel());
+        super.execute();
     }
 
-    @Override
-    public boolean isFinished() {
-        leftPosError.refresh();
-        rightPosError.refresh();
-
-        if (requestPosRots <= 0) {
-            return !_ElevatorSubsystem.getBottomLimitSwitchAsBoolean();
-        } else if (leftPosition.hasUpdated() && rightPosition.hasUpdated()) {
-
-            SmartDashboard.putNumber("Ele Left Pos", leftPosition.getValueAsDouble());
-            SmartDashboard.putNumber("Ele Right", rightPosition.getValueAsDouble());
-
-            return (Math.abs(leftPosition.getValueAsDouble() - (requestPosRots + _ElevatorSubsystem.getLeftHomePos())) <= 0.1
-                    ||
-                    Math.abs(rightPosition.getValueAsDouble()
-                            - (requestPosRots + _ElevatorSubsystem.getRightHomePos())) <= 0.1);
-        }
-
-        return false;
+    /**
+     * Gets the next level of the elevator.
+     * 
+     * @param currentLevel is the current level of the elevator.
+     * @return the next level of the elevator, returns the current level if the
+     *         level of the elevator cannot be increased.
+     */
+    private static ElevatorLevels getLevel(ElevatorLevels currentLevel) {
+        if (validToMoveUp(currentLevel))
+            return ElevatorLevels.values()[currentLevel.ordinal() + 1];
+        return currentLevel;
     }
 
-    @Override
-    public void end(boolean interrupted) {
-        // RobotContainer.noteAquired = false;
-        _ElevatorSubsystem.getLeftMotor().stopMotor();
-        _ElevatorSubsystem.getRightMotor().stopMotor();
-        _ElevatorSubsystem.getLeftMotor().setNeutralMode(NeutralModeValue.Brake);
-        _ElevatorSubsystem.getRightMotor().setNeutralMode(NeutralModeValue.Brake);
-    }
-
-    public static double getEncoderValueForLevel(ElevatorLevels level) {
-        switch (level) {
-            case HOME:
-                return 0.0;
-            case ONE:
-                return 0.0;
-            case TWO:
-                return 0.0;
-            case THREE:
-                return 0.0;
-            case FOUR:
-                return 0.0;
-            default:
-                throw new IllegalArgumentException("Unknown level: " + level);
-        }
+    /**
+     * Checks if the elevator is not at the top level.
+     * 
+     * @param currentLevel The current level of the elevator.
+     * @return true if the elevator is not at the top level, false otherwise.
+     */
+    private static boolean validToMoveUp(ElevatorLevels currentLevel) {
+        return !ElevatorLevels.values()[ElevatorLevels.values().length - 1].equals(currentLevel);
     }
 }
