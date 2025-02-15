ee85e2884e75cf5d2d88f52abfac88c4623e61c4
Nanson Chen <buyingsaucer2@gmail.com>
2025-02-15 18:33:11 -0500

    Improvement on Hand Command.

--- src/main/java/frc/robot/commands/HandCommand.java
+++ src/main/java/frc/robot/commands/HandCommand.java
@@ -4,56 +4,6 @@
 
 package frc.robot.commands;
 
-<<<<<<< HEAD
-
-import frc.robot.subsystems.HandSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
-
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.revrobotics.spark.SparkBase.ControlType;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
- 
-
-public class HandCommand extends Command{
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-
-    HandSubsystem _HandSubsystem;
-    double speed;
-    VelocityVoltage intakeRequest;
-    public DigitalInput handLimitSwitch = new DigitalInput(1);
-   /**
-   * Creates a new HandCommand.
-   *  HandCommand causees the neo in the hand to spin off the given value either intakeing or outakeing
-   * @param subsystem The HandSubsystem.
-   */
-    public HandCommand(HandSubsystem handSubsystem, double intakeSpeed){
-        _HandSubsystem = handSubsystem;
-        speed = intakeSpeed;
-        addRequirements(_HandSubsystem);
-    }
-
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-      _HandSubsystem.setSpeed(speed);
-    }
-
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-    return handLimitSwitch.get();
-    }
-
-    //Ends the movement of the motor
-    @Override
-    public void end(boolean interrupted) {
-        _HandSubsystem.setSpeed(0.0);
-    }
-=======
 import frc.robot.subsystems.HandSubsystem;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -70,7 +20,7 @@ public class HandCommand extends Command {
   /**
    * Creates a new HandCommand.
    * HandCommand causees the neo in the hand to spin off the given value either
-   * intakeing or outakeing
+   * intaking or outaking (positive for intake, negative for outake)
    * 
    * @param subsystem The HandSubsystem.
    */
@@ -92,10 +42,9 @@ public class HandCommand extends Command {
     return handLimitSwitch.get();
   }
 
-  // Ends the movement of the motor
+  // Stops the movement of the motor.
   @Override
   public void end(boolean interrupted) {
     _HandSubsystem.setSpeed(0.0);
   }
->>>>>>> 95165dba422ba333e57e91f8c699ea65f6f075ed
 }
