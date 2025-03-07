e76af512b79772e8e4e31ec2833b6a3858edfaf8
BrickLayer22 <mason.kemp.silva@icloud.com>
2025-03-06 20:03:24 -0500

    Deleated example subsystem

--- src/main/java/frc/robot/commands/ArmDownCommand.java
+++ src/main/java/frc/robot/commands/ArmDownCommand.java
@@ -6,12 +6,9 @@ package frc.robot.commands;
 
 import frc.robot.Constants.ArmPositions;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
-
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.revrobotics.spark.SparkBase.ControlType;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
